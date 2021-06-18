767cee3a650a677585d4ce452ba4af529f3d93bd
Musa Kahya <musakahya@192.168.0.104>
2021-06-18 04:19:18 +0300

    Add Heroku URL to the allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-9#my19kwp%i03gge6fvj)a=kmd6tpj-f#ywzjgf2i7@)%twwq$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'swe-599-watermark.herokuapp.com',
+]
 
 
 # Application definition
