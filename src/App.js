d178d88bb643eae79ac854d85f47a7155758960c
Musa Kahya <musakahya@192.168.0.104>
2021-06-19 04:24:55 +0300

    Add front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,266 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import WbAutoIcon from '@material-ui/icons/WbAuto';
+import axios from 'axios';
 
-function App() {
+import Result from './Result'
+
+function Copyright() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-export default App;
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+  button: {
+    display: 'block',
+    marginTop: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  const [isLoading, setLoading] = React.useState();
+
+  const [audio, setAudio] = React.useState('');
+  const [openAudio, setOpenAudio] = React.useState(false);
+
+  const [image, setImage] = React.useState('');
+  const [openImage, setOpenImage] = React.useState(false);
+
+  const [domain, setDomain] = React.useState('');
+  const [openDomain, setOpenDomain] = React.useState(false);
+
+  const handleChangeAudio = (event) => {
+    setAudio(event.target.value);
+  };
+
+  const handleCloseAudio = () => {
+    setOpenAudio(false);
+  };
+
+  const handleOpenAudio = () => {
+    setOpenAudio(true);
+  };
+
+  const handleChangeImage = (event) => {
+    setImage(event.target.value);
+  };
+
+  const handleCloseImage = () => {
+    setOpenImage(false);
+  };
+
+  const handleOpenImage = () => {
+    setOpenImage(true);
+  };
+
+  const handleChangeDomain = (event) => {
+    console.log(event.target.value);
+    setDomain(event.target.value);
+  };
+
+  const handleCloseDomain = () => {
+    setOpenDomain(false);
+  };
+
+  const handleOpenDomain = () => {
+    setOpenDomain(true);
+  };
+
+  const handleStart = () => {
+    setLoading(true);
+    let urlPath = '';
+    console.log(domain);
+    if(domain === "time") urlPath = "time_domain";
+    else if(domain === "wavelet") urlPath = "wavelet";
+    else if(domain === "cosine") urlPath = "cosine";
+    else;
+    axios.get('https://swe-599-watermark.herokuapp.com/' + urlPath + '/')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <WbAutoIcon fontSize="large" className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Audio Watermarker
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Protect your audio
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Embed secret images into your audio files to claim ownership. Compare embedding in different domains such as <strong>time, cosine,</strong> and <strong>wavelet</strong>. Pick the one that works best.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Get Started
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Learn More
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <FormControl className={classes.formControl} fullWidth>
+            <InputLabel id="demo-controlled-open-select-label">Select Audio</InputLabel>
+            <Select
+              labelId="demo-controlled-open-select-label"
+              id="demo-controlled-open-select"
+              open={openAudio}
+              onClose={handleCloseAudio}
+              onOpen={handleOpenAudio}
+              value={audio}
+              onChange={handleChangeAudio}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={"100grand"}>100grand.wav</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl} fullWidth>
+            <InputLabel id="demo-controlled-open-select-label">Select Image</InputLabel>
+            <Select
+              labelId="demo-controlled-open-select-label"
+              id="demo-controlled-open-select"
+              open={openImage}
+              onClose={handleCloseImage}
+              onOpen={handleOpenImage}
+              value={image}
+              onChange={handleChangeImage}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={"lena"}>lena.bmp</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl} fullWidth>
+            <InputLabel id="demo-controlled-open-select-label">Select Domain</InputLabel>
+            <Select
+              labelId="demo-controlled-open-select-label"
+              id="demo-controlled-open-select"
+              open={openDomain}
+              onClose={handleCloseDomain}
+              onOpen={handleOpenDomain}
+              value={domain}
+              onChange={(e) => handleChangeDomain(e)}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={"time"}>Time</MenuItem>
+              <MenuItem value={"dct"}>Discrete Cosine Transform</MenuItem>
+              <MenuItem value={"dwt"}>Discrete Wavelet Transform</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl} fullWidth>
+            <Button variant="contained" color="primary" onClick={handleStart}>
+              {isLoading === true ? "Working..." : (isLoading === false ? "Completed" : "Start Watermarking")}
+            </Button>
+          </FormControl>
+          {
+            isLoading === true || isLoading === false ?
+              <FormControl className={classes.formControl} fullWidth>
+                <Result isLoading={isLoading} />
+              </FormControl>
+              : ""
+          }
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
---
+++ src/Result.js
@@ -0,0 +1,198 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+
+    },
+    loading: {
+        height: 250,
+    },
+    spinner: {
+        marginBottom: 20,
+    },
+    root: {
+        width: '100%',
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+    chartHeading: {
+        marginBottom: 20,
+    }
+}));
+
+const data = [
+    {
+        "name": "No attack",
+        "SNR (dB)": 4000,
+        "Robustness": 3000,
+    },
+    {
+        "name": "Low Pass Filter",
+        "SNR (dB)": 2000,
+        "Robustness": 3000,
+    },
+    {
+        "name": "Shearing",
+        "SNR (dB)": 1000,
+        "Robustness": 3000,
+    },
+    {
+        "name": "AWGN",
+        "SNR (dB)": 4000,
+        "Robustness": 3000,
+    },
+]
+
+export default function Result({ isLoading }) {
+    const classes = useStyles();
+
+    return (
+        <React.Fragment>
+            <Paper className={classes.paper}>
+                {isLoading ?
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                        className={classes.loading}
+                    >
+                        <CircularProgress className={classes.spinner} />
+                        Please wait while we are processing your audio and input files. This may take a while.
+                    </Grid>
+                    : ""}
+                {!isLoading ?
+                    <Grid
+                        container
+                    >
+                        <div className={classes.root}>
+                            <Accordion defaultExpanded={true}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <Typography className={classes.heading}><strong>Original Image</strong></Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                        sit amet blandit leo lobortis eget.
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                            <Accordion defaultExpanded={true}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel2a-content"
+                                    id="panel2a-header"
+                                >
+                                    <Typography className={classes.heading}><strong>Watermarked Image</strong></Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                        sit amet blandit leo lobortis eget.
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                            <Accordion defaultExpanded={false}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel2a-content"
+                                    id="panel2a-header"
+                                >
+                                    <Typography className={classes.heading}><strong>Watermarked Image Under Low Pass Filter Attack</strong></Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                        sit amet blandit leo lobortis eget.
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                            <Accordion defaultExpanded={false}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel2a-content"
+                                    id="panel2a-header"
+                                >
+                                    <Typography className={classes.heading}><strong>Watermarked Image Under Shearing Attack</strong></Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                        sit amet blandit leo lobortis eget.
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                            <Accordion defaultExpanded={false}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel2a-content"
+                                    id="panel2a-header"
+                                >
+                                    <Typography className={classes.heading}><strong>Watermarked Image Under AWGN Attack</strong></Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                        sit amet blandit leo lobortis eget.
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                            <Accordion defaultExpanded={true}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel3a-content"
+                                    id="panel3a-header"
+                                >
+                                    <Typography className={classes.heading}><strong>Attack Analysis</strong></Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Grid
+                                        container
+                                        direction="column"
+                                        justify="start"
+                                        alignItems="start"
+                                    >
+                                        <Typography className={classes.chartHeading}>SNR</Typography>
+                                        <BarChart width={730} height={250} data={data}>
+                                            <CartesianGrid strokeDasharray="3 3" />
+                                            <XAxis dataKey="name" />
+                                            <YAxis />
+                                            <Tooltip />
+                                            <Legend />
+                                            <Bar dataKey="SNR (dB)" fill="#8884d8" />
+                                        </BarChart>
+                                        <Typography className={classes.chartHeading}>Robustness</Typography>
+                                        <BarChart width={730} height={250} data={data}>
+                                            <CartesianGrid strokeDasharray="3 3" />
+                                            <XAxis dataKey="name" />
+                                            <YAxis />
+                                            <Tooltip />
+                                            <Legend />
+                                            <Bar dataKey="Robustness" fill="#8884d8" />
+                                        </BarChart>
+                                    </Grid>
+                                </AccordionDetails>
+                            </Accordion>
+                        </div>
+                    </Grid>
+                    : ""}
+            </Paper>
+        </React.Fragment>
+    );
+}
